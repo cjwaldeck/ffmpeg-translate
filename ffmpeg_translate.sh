1a0d6e3ab0077d8727059f3eaf5c75ee1101f912
Carl-Johan Waldeck <calle@waldeck.nu>
2021-01-25 18:23:09 +0100

    Add start to ffmpeg_translate script
    It's a start. It currently accepts rtmp streams for input and
    output, and alsa devices for translator input and monitor.
    
    It will mix the normalized translator input audio into the video
    audio while ducking the video audio. The video audio will be played
    in the translator monitor device.
    
    Known issue: If input stream is restarted, timestamps
    are out of sync. This needs to be fixed.

---
+++ ffmpeg_translate.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function usage {
+	echo "
+	Translation utility using ffmpeg to mix in an tramslator into a video feed.
+	Video audio will duck for the translation input.
+
+	Mandatory arguments:
+		-i, --stream_in    Input video stream
+		-o, --stream_out   Output video stream
+		-ti, --trans_in    Translator input alsa device
+		-to, --trans_mon   Translator monitor alsa device
+	"
+}
+
+COMP_GAIN=1
+COMP_THRESHOLD=0.01
+COMP_RATIO=5
+COMP_RELEASE=1000
+COMP_ATTACK=1
+
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+		-i|--stream_in) STREAM_IN=$2 ;;
+		-o|--stream_out) STREAM_OUT=$2 ;;
+		-ti|--trans_in) TRANS_IN=$2 ;;
+		-tm|--trans_mon) TRANS_MON=$2 ;;
+		-h|--help) usage ; exit 0 ;;
+	esac
+	shift
+done
+
+echo "
+Starting translation stream with the following:
+
+    Stream input:        $STREAM_IN
+    Stream output:       $STREAM_OUT
+    Translation input:   $TRANS_IN
+    Translation monitor: $TRANS_MON
+"
+
+ffmpeg -re -i $STREAM_IN -f alsa -i $TRANS_IN -c:v copy -c:a aac -filter_complex \
+    "[0:a]asplit[compin][tmon]; \
+     [1:a]loudnorm,asplit[sc][tnorm]; \
+     [compin][sc]sidechaincompress=threshold=$COMP_THRESHOLD:ratio=$COMP_RATIO: \
+         level_sc=$COMP_GAIN:release=$COMP_RELEASE:attack=$COMP_ATTACK[compout]; \
+     [compout][tnorm]amix" \
+    -f flv $STREAM_OUT \
+    -map '[tmon]' -f alsa $TRANS_IN
