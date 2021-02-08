e7975bc5738c0efea25729b308bb8f300f1b6b28
Carl-Johan Waldeck <calle@waldeck.nu>
2021-02-08 21:01:11 +0100

    Add push script

---
+++ ffmpeg_translate_push.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function usage {
+	echo "
+	Translation utility using ffmpeg to mix in an tramslator into a video feed.
+	Video audio will duck for the translation input.
+
+	Mandatory arguments:
+		-o, --stream_out   Output stream
+		-ti, --trans_in    Translator input alsa device
+	"
+}
+
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+		-o|--stream_out) STREAM_OUT=$2 ;;
+		-ti|--trans_in) TRANS_IN=$2 ;;
+		-h|--help) usage ; exit 0 ;;
+	esac
+	shift
+done
+
+echo "
+Starting translation stream with the following:
+
+    Stream output:       $STREAM_OUT
+    Translation input:   $TRANS_IN
+"
+
+ffmpeg -f alsa -i $TRANS_IN -c:a aac
+    -filter_complex "[1:a]loudnorm;" -f flv $STREAM_OUT
